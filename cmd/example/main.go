15c373f8035fb83d8dfa0c4dfcf2354d9934db3f
Igor Wiedler <igor@travis-ci.org>
2016-10-10 22:37:34 +0200

    working proof-of-concept

---
+++ cmd/example/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/travis-ci/sso"
+)
+
+func main() {
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+			fmt.Printf("[INTERNAL] received header: %v\n", req.Header.Get("Travis-State"))
+			fmt.Fprintf(w, "Welcome to the home page!")
+		})
+		log.Fatal(http.ListenAndServe("127.0.0.1:8081", nil))
+	}()
+
+	upstreamURL, err := url.Parse("http://127.0.0.1:8081")
+	if err != nil {
+		panic(err)
+	}
+
+	apiURL, err := url.Parse("https://api.travis-ci.org")
+	if err != nil {
+		panic(err)
+	}
+
+	appPublicURL, err := url.Parse("http://localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	sso := &sso.SSO{
+		UpstreamURL:   upstreamURL,
+		APIURL:        apiURL,
+		AppPublicURL:  appPublicURL,
+		PublicPath:    "public",
+		EncryptionKey: []byte("sa8OoLei6eWiezah9ohk8Wah6Ow6pee9"),
+		Authorized: func(u sso.User) (bool, error) {
+			return true, nil
+		},
+	}
+
+	s := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: sso,
+	}
+	s.ListenAndServe()
+}
--- sso.go
+++ sso.go
@@ -1,19 +1,32 @@
 package sso
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"time"
+
+	"github.com/vulcand/oxy/forward"
 )
 
-type Options struct {
-	UpstreamURL string
-	APIURL      string
-	PublicPath  string
-	Authorized  func(User) (bool, error)
+type SSO struct {
+	UpstreamURL   *url.URL
+	APIURL        *url.URL
+	AppPublicURL  *url.URL
+	PublicPath    string
+	EncryptionKey []byte
+	Authorized    func(User) (bool, error)
 }
 
 type User struct {
@@ -32,8 +45,8 @@ type APIMessage struct {
 	User User
 }
 
-type SSOState struct {
-	User User
+type State struct {
+	User  User
 	Token string
 }
 
@@ -47,66 +60,131 @@ func init() {
 	}
 }
 
-func (o Options) HandleRequest(req http.Request, w *http.ResponseWriter) {
-	if strings.HasPrefix(req.URL.Path, "/__travis__") {
-		return o.handleStatic(req, w)
-	}
+// rewrite request for forwarding
+func (sso SSO) Rewrite(r *http.Request) {
 
-	if isLogin(req) {
-		return o.handleLogin(req, w)
-	}
-
-	if isLogout(req) {
-		return o.handleLogout(req, w)
-	}
-
-	if isAuthenticated(req) {
-		return o.handleProxy(req, w)
-	}
-
-	if isHandshake(req) {
-		return o.handleHandshake(req, w)
-	}
 }
 
-func (o Options) handleStatic(req http.Request, w *http.ResponseWriter) {
+func (sso SSO) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("ServeHTTP")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/favicon.ico", sso.handleEmpty)
+	mux.HandleFunc("/foo", sso.handleHello)
+	mux.HandleFunc("/bar", sso.handleHello)
+	mux.Handle("/sso/static/", sso.handleStatic(w, req))
+	mux.HandleFunc("/sso/login", sso.handleLogin)
+	mux.HandleFunc("/sso/logout", sso.handleLogout)
+	mux.HandleFunc("/", http.HandlerFunc(sso.handleRequest))
+
+	mux.ServeHTTP(w, req)
 }
 
-func (o Options) handleProxy(req http.Request, w *http.ResponseWriter) {
+func (sso SSO) handleEmpty(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(204)
 }
 
-func (o Options) handleLogin(req http.Request, w *http.ResponseWriter) {
-	token, err := ssoToken(request)
+func (sso SSO) handleHello(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("hello"))
+}
+
+func (sso SSO) handleStatic(w http.ResponseWriter, req *http.Request) http.Handler {
+	return http.StripPrefix("/sso/static/", http.FileServer(http.Dir(sso.PublicPath)))
+}
+
+func (sso SSO) handleRequest(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("handleRequest")
+
+	state, err := sso.stateFromRequest(req)
+	if err != nil && err != http.ErrNoCookie {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if state != nil {
+		// we have a state => we are authenticated
+		sso.handleProxy(w, req, state)
+		return
+	}
+
+	sso.handleHandshake(w, req)
+}
+
+func (sso SSO) handleProxy(w http.ResponseWriter, req *http.Request, state *State) {
+	fmt.Println("handleProxy")
+
+	b, err := json.Marshal(state)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
-	url = o.APIURL + "/users?access_token=%s"
-	apiReq, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
+	req.URL.Scheme = sso.UpstreamURL.Scheme
+	req.URL.Host = sso.UpstreamURL.Host
+	req.Header.Add("Travis-State", string(b))
+
+	fwd, _ := forward.New()
+	fwd.ServeHTTP(w, req)
+}
+
+func (sso SSO) handleLogin(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("handleLogin")
+
+	token := ""
+	if req.Method == "POST" {
+		token = req.FormValue("sso_token")
 	}
+
+	if token == "" {
+		log.Println("no token found, try again")
+		http.Redirect(w, req, "/", http.StatusFound)
+		return
+	}
+
+	url := *sso.APIURL
+
+	q := url.Query()
+	q.Add("access_token", token)
+
+	url.Path = "/users"
+	url.RawQuery = q.Encode()
+
+	apiReq, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	client := &http.Client{}
 
 	apiReq.Header.Add("Accept", "application/vnd.travis-ci.2+json")
-	apiResp, err := client.Do(req)
+	apiResp, err := client.Do(apiReq)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), 500)
+		return
 	}
 	defer apiResp.Body.Close()
 
 	// TODO: check response code
+	if apiResp.StatusCode != http.StatusOK {
+		content, _ := ioutil.ReadAll(apiResp.Body)
+		http.Error(w, fmt.Sprintf("upstream error, code=%v, body=%v\n", apiResp.StatusCode, string(content)), 500)
+		return
+	}
 
 	var m APIMessage
 	err = json.NewDecoder(apiResp.Body).Decode(&m)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
 	user := m.User
 
-	ok, err := o.Authorized(user)
+	ok, err := sso.Authorized(user)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
 	if !ok {
@@ -114,28 +192,48 @@ func (o Options) handleLogin(req http.Request, w *http.ResponseWriter) {
 		return
 	}
 
-	// TODO: generate signed cookie containing user
-	ssoState = &SSOState{
-		User: user,
+	state := &State{
+		User:  user,
 		Token: token,
 	}
 
-	b, err := json.Marshal(user)
+	b, err := json.Marshal(state)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
-	encryptedCookie = encrypt(b)
+	encryptedCookie, nonce, err := encrypt(b, sso.EncryptionKey)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 
-	httpSetCookie(w, &http.Cookie{
-		Name:  "travis.sso",
-		Value: signedCookie,
+	encryptedCookie = append(nonce, encryptedCookie...)
+	encodedCookie := base64.StdEncoding.EncodeToString(encryptedCookie)
+
+	domain := sso.AppPublicURL.Host
+	index := strings.Index(domain, ":")
+	if index > 0 {
+		domain = domain[:index]
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "travis.sso",
+		Value:   encodedCookie,
+		Path:    "/",
+		Domain:  domain,
+		Expires: time.Now().Add(365 * 24 * time.Hour),
 	})
 
-	return ssoState, nil
+	fmt.Println("cookies set, redirecting back")
+
+	http.Redirect(w, req, "/", http.StatusFound)
 }
 
-func (o Options) handleHandshake(req http.Request, w *http.ResponseWriter) {
+func (sso SSO) handleHandshake(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("handleHandshake")
+
 	if req.Method != "GET" && req.Method != "HEAD" {
 		w.Header().Set("Allow", "GET, HEAD")
 		http.Error(w, fmt.Sprintf(`must be <a href="%s">GET</a> request`, req.URL), 405)
@@ -143,29 +241,67 @@ func (o Options) handleHandshake(req http.Request, w *http.ResponseWriter) {
 	}
 
 	t.Execute(w, map[string]string{
-		"Public":   o.PublicPath,
-		"Endpoint": o.APIURL,
-		"Origin":   r.URL,
+		"Public":   "/sso/static",
+		"Endpoint": sso.APIURL.String(),
+		"Origin":   sso.AppPublicURL.String(),
 		"CSRF":     "",
 	})
 }
 
-func (o Options) handleLogout(req http.Request, w *http.ResponseWriter) {
-	httpSetCookie(w, &http.Cookie{
-		Name:  "travis.sso",
-		Value: "",
-		Expires: time.Date(1970, time.January, 1, 1, 0, 0, 0, time.UTC)
+func (sso SSO) handleLogout(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("handleLogout")
+
+	domain := sso.AppPublicURL.Host
+	index := strings.Index(domain, ":")
+	if index > 0 {
+		domain = domain[:index]
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "travis.sso",
+		Value:   "",
+		Path:    "/",
+		Domain:  domain,
+		Expires: time.Date(1970, time.January, 1, 1, 0, 0, 0, time.UTC),
 	})
 
 	w.Write([]byte("logged out"))
 }
 
-func ssoToken(req http.Request) {
-	if req.Method == "POST" {
-		return req.FormValue("sso_token")
+func (sso SSO) stateFromRequest(req *http.Request) (*State, error) {
+	cookie, err := req.Cookie("travis.sso")
+	if err == http.ErrNoCookie {
+		return nil, http.ErrNoCookie
 	}
-	return ""
+	if err != nil {
+		return nil, err
+	}
+
+	decodedCookie, err := base64.StdEncoding.DecodeString(cookie.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	encryptedCookie := []byte(decodedCookie)
+
+	nonce := encryptedCookie[:12]
+	encryptedCookie = encryptedCookie[12:]
+
+	b, err := decrypt(encryptedCookie, nonce, sso.EncryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	var state *State
+	err = json.NewDecoder(bytes.NewReader(b)).Decode(&state)
+	if err != nil {
+		return nil, err
+	}
+
+	return state, nil
 }
+
+// https://gist.github.com/kkirsche/e28da6754c39d5e7ea10
 
 func encrypt(plaintext, key []byte) ([]byte, []byte, error) {
 	block, err := aes.NewCipher(key)
